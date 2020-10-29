d33232114db9a17bdd220303c6a88c12b670d1cd
Rajasekhar <itsraj@ymail.com>
2020-10-29 20:42:53 +0530

    Update site.js

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,8 +1,8 @@
 module.exports = {
-    title: "Vredeburg",
+    title: "Rajasehar11ty",
     description: "Eleventy starter project to create a blog",
-    keywords: ["eleventy", "template", "simple", "clean"],
-    url: "https://vredeburg.netlify.app", // don't include trailing slash!
+    keywords: ["rajasekhar", "11ty"],
+    url: "https://raj11ty.netlify.app", // don't include trailing slash!
     paginate: 6, // how many posts do you want for each page
     // disqusShortname: "your-shortname" // only if you want to add disqus to your site
 }
