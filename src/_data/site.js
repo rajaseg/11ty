0794b6ddf10135caabb6e58da0d0b8726410e89f
Rajasekhar <itsraj@ymail.com>
2020-10-29 20:45:40 +0530

    Update site.js

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,6 +1,6 @@
 module.exports = {
-    title: "Rajasehar11ty",
-    description: "Eleventy starter project to create a blog",
+    title: "Rajasekhar11ty",
+    description: "Eleventy based blog",
     keywords: ["rajasekhar", "11ty"],
     url: "https://raj11ty.netlify.app", // don't include trailing slash!
     paginate: 6, // how many posts do you want for each page
